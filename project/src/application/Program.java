fbb1b19fb815be79500aeafe25c9f8af13493ff1
Lucas <lucasangelloti@gmail.com>
2025-08-14 09:42:54 -0300

    Quarto commit
    primeiras lógicas testadas e trycatch para evitar alguns erros, agora irei trabalhar e depois irei criar um database

--- project/src/application/Program.java
+++ project/src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import model.Cat;
+import model.Medicamento;
 
 import java.util.Scanner;
 
@@ -8,12 +9,24 @@ public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        try{
         System.out.println("Digite a idade do animal: ");
         Integer age = sc.nextInt();
         System.out.println("Digite o peso do animal: ");
         Double weight = sc.nextDouble();
         Cat cat = new Cat(age, weight);
 
+        System.out.println("Digite o nome do medicamento: ");
+        String name = sc.next();
+        System.out.println("Digite a unidade do medicamento: ");
+        String unity = sc.next();
+        System.out.println("Digite a dose por kilograma do medicamento: ");
+        Double dosePerKg = sc.nextDouble();
+        Medicamento medicamento = new Medicamento(name, dosePerKg, unity);
 
+        System.out.println("O valor da dose do animal é: " + cat.calculateDose(medicamento));
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
--- project/src/model/Cat.java
+++ project/src/model/Cat.java
@@ -5,9 +5,14 @@ public class Cat extends Animal{
     public Cat(Integer age, Double weight) {
         super(age, weight);
     }
+    public Cat() {
+        super(null, null);
+    }
 
     @Override
     public Double calculateDose(Medicamento medicamento) {
+        if(getWeight() <= 0)throw new IllegalArgumentException("O peso do animal deve ser maior que zero.");
+        if(medicamento.getDosePerKg() <= 0)throw new IllegalArgumentException("A dose por kilograma do medicamento deve ser maior que zero.");
         return getWeight() * medicamento.getDosePerKg();
     }
 
